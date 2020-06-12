f4a763b553cc0f4762dc0d05f04ed0be477730dc
Brandon-Harris <brandonharris177@gmail.com>
2020-06-12 11:49:51 -0600

    MVP met

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,39 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # previous = None
+        # current = self.head
+        # following = self.head
+
+        # while current is not None:
+        #     following = following.next_node
+        #     current.next_node = previous
+        #     previous = current
+        #     current = following
+        # self.head = previous
+        # return previous
+
+        if node is None:
+            return 
+        elif node.next_node is None:
+            self.head = node 
+            return
+        else:
+            self.reverse_list(node.next_node, None) 
+            node.next_node.next_node = node 
+            node.next_node = None
+
+
+
+    # LList = LinkedList()
+    # LList.add_to_head(1)
+    # LList.add_to_head(2)
+    # LList.add_to_head(3)
+    # LList.add_to_head(4)
+    # LList.add_to_head(5)
+    # print(LList.head.value, 5)
+    # LList.reverse_list(LList.head, None)
+    # print(LList)
+    # assertEqual(list.head.value, 1)
+    # assertEqual(list.head.get_next().value, 2)
+    # assertEqual(list.head.get_next().get_next().value, 3)
